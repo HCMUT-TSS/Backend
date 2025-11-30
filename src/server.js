6ef86fc0e5ebdb6cc082c1bf5613675427f348b3
Phuc2606 <phuc26062005@gmail.com>
2025-11-30 09:53:34 +0700

    Fix connect FE + mockdata

--- src/server.js
+++ src/server.js
@@ -9,10 +9,19 @@ import tutorRoutes from './routes/tutor.js';
 import studentRoutes from './routes/student.js';
 
 const app = express();
+app.use((req, res, next) => {
+  console.log(`➡️  Request vào BE: ${req.method} ${req.url}`);
+  next();
+});
 const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',    
+  credentials: true,
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use("/api/auth", authRoutes);
 app.use('/api/admin', adminRoutes);
